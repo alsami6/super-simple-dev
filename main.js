94db2d4bcbfe3d0d066ef92d26756ea245691a22
Al <alsami1209@gmail.com>
2024-03-29 12:30:28 +0600

    test

---
+++ main.js
@@ -0,0 +1,40 @@
+// let citizen = true;
+// let age = 22;
+// let regs = true;
+
+// if(age >= 18 && citizen===true && regs === true){
+//     console.log('Yes you can vote');
+// }
+// else if(age >= 18 && regs===true && citizen===false){
+//     console.log('You are not citizen of our country');
+// }
+// else if(age >= 18 && regs===false && citizen===true){
+//     console.log("You need to regeisterd first to vote");
+// }
+// else if(age < 18 && citizen===true && regs === true){
+//     console.log("You are under age");
+// }
+// else{
+//     console.log("You can not vote");
+// }
+
+
+// let num = 20;
+
+// if(num % 2 == 0){
+//     console.log('even')
+// }else{
+//     console.log("odd");
+// }
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// // fruits[4] = "Pineapple";
+// // fruits[5] = "Jackfruit"
+
+// // console.log(fruits[fruits.length-1]);
+
+// for (let index = 0; index < fruits.length; index++) {
+//     console.log(fruits[index]);    
+// }
